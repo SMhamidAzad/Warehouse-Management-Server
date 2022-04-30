93ff5d32280c7b0885390531a8c812e8e585e3d9
SMhamidAzad <hamidazad020@gmail.com>
2022-04-30 19:57:11 +0600

    api for single inventory done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,12 +21,21 @@ async function run(){
      await client.connect();
      const productCollection = client.db('tilesWarehouse').collection('product')
 
+    //  api for all product 
      app.get('/product', async(req, res)=>{
        const query = {};
        const cursor = productCollection.find(query);
        const allProduct = await cursor.toArray();
        res.send(allProduct)
      })
+
+    //  api for single product 
+     app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+  });
   }
   finally{
 
