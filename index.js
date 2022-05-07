7925be638c11ec08eb36fb4ddfc783b0b897bf0b
SMhamidAzad <hamidazad020@gmail.com>
2022-05-07 21:04:56 +0600

    implement jwt token for email

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -19,16 +20,44 @@ async function run() {
     await client.connect();
     const productCollection = client.db('tilesWarehouse').collection('product')
 
+    // AUTH token 
+    app.post('/gettoken', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '2d'
+      });
+      res.send({ accessToken })
+    })
+    // product api 
     app.get('/product', async (req, res) => {
       const email = req.query.email;
-      if(email){
+      if (email) {
+        const authHeaders = req.headers.authorization;
 
-        const query = { email: email };
-        const cursor = productCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
+        if (!authHeaders) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        const token = authHeaders.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+          }
+          console.log('decoded', decoded);
+          req.decoded = decoded;
+        })
+
+        const decodedEmail = req.decoded.email;
+        if (decodedEmail === email) {
+          const query = { email: email };
+          const cursor = productCollection.find(query);
+          const result = await cursor.toArray();
+          res.send(result);
+        }
+        else{
+          res.status(403).send({message: 'forbidden access'})
+        }
       }
-      else{
+      else {
         const query = {};
         const cursor = productCollection.find(query);
         const allProduct = await cursor.toArray();
@@ -44,36 +73,36 @@ async function run() {
       res.send(product);
     })
 
-      // delivered product and update quantity
-      app.put('/product/:id', async (req, res) => {
-        const id = req.params.id;
-        const data = req.body;
-        const filter = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updatedDoc = {
-          $set: {
-            quantity: data.newQuantity
-          }
-        };
-        const result = await productCollection.updateOne(filter, updatedDoc, options);
-        res.send(result);
-      })
+    // delivered product and update quantity
+    app.put('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: data.newQuantity
+        }
+      };
+      const result = await productCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
 
-      // added new inventory 
-      app.post('/product', async (req, res) => {
-        const newInventory = req.body;
-        console.log("Successfully adding new inventory", newInventory);
-        const result = await productCollection.insertOne(newInventory);
-        res.send(result)
-      });
+    // added new inventory 
+    app.post('/product', async (req, res) => {
+      const newInventory = req.body;
+      console.log("Successfully adding new inventory", newInventory);
+      const result = await productCollection.insertOne(newInventory);
+      res.send(result)
+    });
 
-      // delete api 
-      app.delete('/product/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await productCollection.deleteOne(query);
-        res.send(result);
-    });  
+    // delete api 
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   }
   finally {
 
@@ -89,3 +118,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Listening to port ', port);
 })
+
+
+
+// https://blooming-hollows-74511.herokuapp.com/
